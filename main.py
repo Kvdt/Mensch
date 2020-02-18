fc0f667b591c16ff7251b082c86a1f296867173d
Kolja.vdt <kolja.vdt@users.noreply.github.com>
2020-02-18 18:08:15 +0100

    added class and all parameters

---
+++ main.py
@@ -0,0 +1,7 @@
+class Mensch:
+    def __init__(self, name:str, height:int, age:int, weight:int, eyecolor:str, isMale:bool):
+        '''
+        Todo:
+        Methode sag hallo
+            hallo, ich bin Name, ich bin Größe meter groß habe Augenfarbe augen...
+        '''
