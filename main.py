e1e74e7303aa45f93c5aa232610a1ba71fe90dec
Kolja.vdt <kolja.vdt@users.noreply.github.com>
2020-02-18 18:15:34 +0100

    finished the code

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 class Mensch:
-    def __init__(self, name:str, height:int, age:int, weight:int, eyecolor:str, isMale:bool):
+    def __init__(self, name:str, height:int, age:int, weight:float, eyecolor:str, isMale:bool):
         '''
-        Todo:
-        Methode sag hallo
-            hallo, ich bin Name, ich bin Größe meter groß habe Augenfarbe augen...
+        :param name: Name des Menschen
+        :param height: Größe des Menschen in cm
+        :param age: Alter des Menschen in gazen Jahren
+        :param weight: Gewicht des Menschen in Kg
+        :param eyecolor: Farbe der Augen
+        :param isMale: ob der Mensch männlich ist
         '''
+        self.name = name
+        self.height = height
+        self.age = age
+        self.weight = weight
+        self.eyecolor = eyecolor
+        self.isMale = isMale
+
+    def sayHello(self):
+        return (
+                'Hallo, mein Name ist ' + self.name +
+                ' ich bin ' + str(self.age) +
+                ' Jahre alt, und bin ' + str(self.height/100) +
+                ' meter Groß.'
+                )
+
+Kolja = Mensch('Kolja', 179, 15, 59.08, 'Braun', True)
+print(Kolja.sayHello())
